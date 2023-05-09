f441556bdad96da99a3d9256f4ae5d8b86541116
gisearcuri <gise.arcuri@hotmail.com.ar>
2023-05-09 20:00:57 -0300

    Se hizo el footer y modificaron logo redes y se agrego boton ingreso

--- PI_Gisela_Arcuri/src/app/app.module.ts
+++ PI_Gisela_Arcuri/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import  {  NgCircleProgressModule  }  from  'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
+    FooterComponent,
     
   ],
   imports: [
