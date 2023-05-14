36c89ce2a7e40945569f09e0ae1c44a075a9cab1
gisearcuri <gise.arcuri@hotmail.com.ar>
2023-05-14 20:44:38 -0300

    Se cargaron datos personales

---
+++ PI_Gisela_Arcuri/BackEnd/src/main/java/com/portfolio/gise/GiseApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiseApplication.class, args);
+	}
+
+}
---
+++ PI_Gisela_Arcuri/BackEnd/src/test/java/com/portfolio/gise/GiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.gise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
